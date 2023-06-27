1a3f2c301213c1bf6f423480fd278a177153e58d
tungbt <tungbuivn@gmail.com>
2023-06-27 08:48:21 +0700

    add readme

--- GCodeProcess/AppOptions.cs
+++ GCodeProcess/AppOptions.cs
@@ -1,5 +1,6 @@
 ﻿using CommandLine;
 
+
 public class AppOptions
 {
     [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
@@ -8,5 +9,6 @@ public class AppOptions
     [Value(0, MetaName = "cmd", HelpText = "command line process.ger,360")]
     public string Cmd { get; set; } = "ger";
     
-   
+    [Option('t', "top", Required = false, HelpText = "process top layer.")]
+    public int Top { get; set; } = 0;
 }
