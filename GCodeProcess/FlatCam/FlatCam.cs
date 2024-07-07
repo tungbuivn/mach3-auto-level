16b5d84f8c411600f8d52b1b8364d79c93fe7b74
tungbuivn <tungbuivn@gmail.com>
2024-07-07 18:41:44 +0700

    fix params

--- GCodeProcess/FlatCam/FlatCam.cs
+++ GCodeProcess/FlatCam/FlatCam.cs
@@ -89,6 +89,13 @@ write_gcode ncc_bottom_layer {dir}/gb_bottom_layer.nc
             : "";
         var scriptTemplate = $@"
 set_sys {q}units{q} {q}MM{q}
+set_sys {q}cncjob_plot{q} false
+set_sys {q}tools_drill_multidepth{q} false
+set_sys {q}tools_drill_drill_slots{q} true
+set_sys {q}tools_drill_last_drill{q} false
+#set_sys {q}geometry_plot{q} false
+set_sys {q}tools_drill_depthperpass{q} 5
+set_sys {q}tools_drill_cutz{q} -2.2
 open_gerber {dir}/Gerber_BoardOutlineLayer.GKO -outname cutout
 open_gerber {dir}/Gerber_BottomLayer.GBL -outname bottom_layer
 #open top layer gerber
